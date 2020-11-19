99c2c90bca956089a0835b54ed8ae2bff3fd43dc
imtiaz17 <imtcse@gmail.com>
2020-11-19 15:25:42 +0600

    Inhance product schema

--- src/models/product.js
+++ src/models/product.js
@@ -1,41 +1,51 @@
-const mongoose = require('mongoose');
-const productSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        trim:true,
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    slug:{
-        type:String,
-        required:true,
-        unique:true,
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    price:{
-        type:Number,
-        required:true
+    price: {
+      type: Number,
+      required: true,
     },
-    quantity:{
-        type:Number,
-        required:true
+    sell_price: { type: Number },
+    quantity: {
+      type: Number,
+      required: true,
     },
-    description:{
-        type:String,
-        required:true,
-        trim:true,
+    sku: {
+      type: String,
     },
-    offer:{type:Number},
-    productPic:[
-        {img:{type:String}}
+    tags: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    productPic: [{ img: { type: String } }],
+    reviews: [
+      {
+        userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        rating: Number,
+        review: String,
+      },
     ],
-    reviews:[
-        {
-            userId: {type:mongoose.Schema.Types.ObjectId, ref: 'User'},
-            rating: Number,
-            review:String
-        }
-    ],
-    category:{type:mongoose.Schema.Types.ObjectId,ref:'Category',required:true}
-    
-},{timestamps:true});
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Product',productSchema);
+module.exports = mongoose.model("Product", productSchema);
