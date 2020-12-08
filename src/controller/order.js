29a111e634c9b83431793609db02cffea7b0a451
imtiaz17 <imtcse@gmail.com>
2020-12-09 02:06:32 +0600

    update order status

--- src/controller/order.js
+++ src/controller/order.js
@@ -12,6 +12,18 @@ exports.addOrder = (req, res) => {
           date: new Date(),
           isCompleted: true,
         },
+        {
+          type: "packed",
+          isCompleted: false,
+        },
+        {
+          type: "shipped",
+          isCompleted: false,
+        },
+        {
+          type: "delivered",
+          isCompleted: false,
+        },
       ];
       const order = new Order(req.body);
       order.save((error, order) => {
@@ -34,4 +46,25 @@ exports.getOrders = (req, res) => {
         res.status(200).json({ orders });
       }
     });
+};
+exports.updateOrderStatus = (req, res) => {
+  Order.updateOne(
+    {
+      _id: req.body.orderId,
+      "orderStatus.type": req.body,
+      type,
+    },
+    {
+      $set: {
+        "orderStatus.$": [
+          { type: req.body.type, date: new Date(), isCompleted: true },
+        ],
+      },
+    }
+  ).exec((error, order) => {
+    if (error) return res.status(400).json({ error });
+    if (order) {
+      res.status(200).json({ order });
+    }
+  });
 };
--- src/routes/order.js
+++ src/routes/order.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {requireSignin,onlyaccess} = require ('../middleware/auth')
-const {addOrder,getOrders}= require ('../controller/order')
+const {addOrder,getOrders,updateOrderStatus}= require ('../controller/order')
 
 
 router.post("/addOrder", requireSignin, addOrder);
 router.get("/getOrders", requireSignin, getOrders);
+router.post("/update_order_status", requireSignin, updateOrderStatus);
 
 module.exports = router;
